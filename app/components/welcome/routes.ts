adb81f65877cc22d5bac02e177b9498af05eba77
ASDAlexey <asdalexey@yandex.ru>
2016-01-10 18:19:52 +0300

    add html5Mode router

--- app/components/welcome/routes.ts
+++ app/components/welcome/routes.ts
@@ -1,11 +1,16 @@
 import Controller from './welcome.controller';
 let Template = require('./assets/templates');
-export function config($stateProvider:angular.ui.IStateProvider):void {
+export function config($stateProvider:angular.ui.IStateProvider,$locationProvider,$urlRouterProvider):void {
     "ngInject";
-    $stateProvider.state('welcome', {
-        url: "/",
-        template: Template,
-        controller: Controller,
-        controllerAs: 'vm'
-    });
+$stateProvider.state('welcome', {
+    url: "/asd",
+    template: Template,
+    controller: Controller,
+    controllerAs: 'vm'
+});
+$locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+}).hashPrefix('!');
+$urlRouterProvider.otherwise('/');
 }
